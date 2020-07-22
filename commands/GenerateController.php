1005ef001ca1483ac0598cb97d3662008fa91100
forecho <caizhenghai@gmail.com>
2020-07-22 15:22:49 +0800

    ci: use github action ci

---
+++ commands/GenerateController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\helpers\Console;
+use yiier\helpers\Security;
+
+class GenerateController extends Controller
+{
+    public function actionKey()
+    {
+        $environmentFilePath = \Yii::getAlias('@app/.env');
+        foreach (['COOKIE_VALIDATION_KEY', 'JWT_SECRET'] as $item) {
+            $escaped = preg_quote('=' . env($item), '/');
+            $keyReplacementPattern = "/^{$item}{$escaped}/m";
+            $key = Security::generateRealUniqId(32);
+            file_put_contents($environmentFilePath, preg_replace(
+                $keyReplacementPattern,
+                "{$item}={$key}",
+                file_get_contents($environmentFilePath)
+            ));
+            $this->stdout("{$item} key [{$key}] set successfully.\n", Console::FG_GREEN);
+        }
+    }
+}
