e36ba8f9b1f1da4e7d250de88b7dbc328154d640
forecho <caizhenghai@gmail.com>
2020-07-23 09:49:46 +0800

    fix ci

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
+define('DOTENV_FILE', '.env.testing');
 
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../vendor/autoload.php';
