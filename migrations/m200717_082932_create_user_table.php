2cb7a5fee8b083683f15f43cb54606ddf0d57093
forecho <caizhenghai@gmail.com>
2020-07-23 11:55:06 +0800

    fix: migrate

--- migrations/m200717_082932_create_user_table.php
+++ migrations/m200717_082932_create_user_table.php
@@ -21,8 +21,8 @@ class m200717_082932_create_user_table extends Migration
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->defaultValue(''),
             'status' => $this->tinyInteger()->defaultValue(0),
-            'created_at' => $this->timestamp(),
-            'updated_at' => $this->timestamp(),
+            'created_at' => $this->timestamp()->defaultValue(null),
+            'updated_at' => $this->timestamp()->defaultValue(null),
         ]);
     }
 
